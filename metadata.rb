c7a57b2310308d46424f27bdf46dad9476da0f92
kogecoo <kogecoo@gmail.com>
2015-02-28 01:39:04 +0900

    bump version to 0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kogecoo@gmail.com'
 license          'Apache 2.0'
 description      'Installs an official java binding for mecab' 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'java'
 depends 'mecab'
